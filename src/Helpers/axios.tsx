971da08b0ffc1ebf8778f7a180875e2dc92bf04f
Jane49-cloud <janendirangu49@gmail.com>
2023-12-01 22:07:27 +0300

    debug

--- src/Helpers/axios.tsx
+++ src/Helpers/axios.tsx
@@ -1,27 +1,28 @@
-import axios from "axios";
-import { getAccessToken } from "../hooks/user.actions";
+import axios from 'axios';
+import { getAccessToken } from '../hooks/user.actions';
 
 // https://bloghub-p25a.onrender.com
 
 const axiosService = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    // baseURL: "http://localhost:8000/api/v1",
+    baseURL: 'https://bloghub-p25a.onrender.com',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 axiosService.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${getAccessToken()}`;
-  return config;
+    config.headers.Authorization = `Bearer ${getAccessToken()}`;
+    return config;
 });
 
 axiosService.interceptors.response.use(
-  (response) => response,
-  (error) => Promise.reject(error)
+    (response) => response,
+    (error) => Promise.reject(error)
 );
 
 export function fetcher(url: string) {
-  return axiosService.get(url).then((response) => response.data);
+    return axiosService.get(url).then((response) => response.data);
 }
 
 export default axiosService;
