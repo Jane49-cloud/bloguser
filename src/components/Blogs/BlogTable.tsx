98b1cb9acaa009d247b7f514d6ae942279f5e5cb
Jane49-cloud <janendirangu49@gmail.com>
2024-01-08 20:21:04 +0300

    blog table global

---
+++ src/components/Blogs/BlogTable.tsx
@@ -0,0 +1,53 @@
+import { postProps } from "@/Interfaces/post";
+import React from "react";
+import { FaRegEye, FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+
+interface Props {
+  blogs: postProps;
+}
+
+const BlogTable: React.FC<Props> = ({ blogs }) => {
+  return (
+    <div>
+      <table className="no-scrollbar min-w-full table-auto overflow-x-auto bg-white  shadow-md">
+        <thead>
+          <tr className="w-[100%] bg-teal-500">
+            <th className="px-4 py-2 font-bold">Title</th>
+            <th className="px-4 py-2 font-bold">Description</th>
+            <th className="px-4 py-2 font-bold">Author</th>
+            <th className="px-4 py-2 font-bold">Date</th>
+            <th className="px-4 py-2 font-bold">Status</th>
+            <th className="px-4 py-2 font-bold">Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {blogs?.map((blog: any) => (
+            <tr key={blog._id}>
+              <td className="px-4 py-2">{blog?.title}</td>
+              <td className="px-4 py-2">{blog?.description}</td>
+              <td className="px-4 py-2">{`${blog?.author.firstName} ${blog?.author.lastName}`}</td>
+              <td className="px-4 py-2">
+                {new Date(blog?.createdAt).toDateString()}
+              </td>
+              <td className="px-4 py-2">{blog?.status}</td>
+              <td className="px-4 py-2">
+                <button className="mx-1 text-blue-500 hover:text-blue-700">
+                  <FaRegEye />
+                </button>
+                <button className="mx-1 text-green-500 hover:text-green-700">
+                  <FaRegEdit />
+                </button>
+                <button className="mx-1 text-red-500 hover:text-red-700">
+                  <FaRegTrashAlt />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BlogTable;
